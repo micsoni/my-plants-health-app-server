838b598891c4b58272e4715b2e98adc74a7a37d2
micsoni <micsoni@gmail.com>
2020-03-25 16:23:05 +0100

    added auth

---
+++ auth/router.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const { toJWT } = require("./jwt");
+const User = require("../user/model");
+const bcrypt = require("bcrypt");
+
+const router = new Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const loginInfo = req.body;
+    if (!loginInfo.email || !loginInfo.password || !loginInfo.username) {
+      res.status(400).send({message:"Please supply a valid name, email and password"});
+    } else {
+      const userFound = await User.findOne({
+        where: {
+          email: loginInfo.email
+        }
+      });
+      if (!userFound) {
+        res.status(400).send({message:"User with that email does not exist"});
+      } else if (bcrypt.compareSync(loginInfo.password, userFound.password)) {
+        res.send({
+          jwt: toJWT({ userId: userFound.id }),
+          id: userFound.id,
+          name: userFound.username
+        });
+      } else {
+        res.status(400).send({message:"Password was incorrect"});
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
