9a78f5eb5c33ea3b21a0ae56bf28174ab92a0de0
micsoni <micsoni@gmail.com>
2020-03-25 23:18:10 +0100

    added alamr and note endpoints

---
+++ alarm/router.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const auth = require("../auth/middleware");
+const authorize = require("../user/middleware");
+const Alarm = require("./model")
+
+const router = express.Router();
+
+router.post("/alarm", auth, authorize, async (req, res, next) => {
+  try {
+    if (!req.body.name || !req.body.frequency || !req.body.time) {
+      res.status(400).send({
+        message: "Please define a name, frequency and time"
+      });
+    } else {
+      const postAlarm = await Alarm.create(req.body);
+      res.send(postAlarm);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+router.put("/alarm/:alarmId", auth, authorize, 
+async (req, res, next) => {
+    try {
+      if (!req.body.name || !req.body.frequency || !req.body.time) {
+        res.status(400).send({
+          message:  "Please define a name, frequency and time"
+        });
+      } else {
+        const alarm = await Alarm.findByPk(req.params.alarmId);
+        const updated = await alarm.update(req.body);
+        res.send(updated);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete("/alarm/:alarmId", auth, authorize,
+  async (req, res, next) => {
+    try {
+      const number = await Alarm.destroy({ where: { id: req.params.alarmId } });
+      if (number === 0) {
+        res.status(404).send({ message: "No alarm found" });
+      } else {
+        res.status(202).json(number);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
--- user/middleware.js
+++
@@ -1,7 +0,0 @@
-function authorize(req, res, next) {
-  if (req.user.dataValues.id !== req.params.userId) {
-    return res.status(401).send({ message: "User not authorized" });
-  }
-  next();
-}
-module.exports = authorize;
